354be93fcb01f67b8a0d39a65ca9cf063dd2ca5a
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-24 14:59:30 -0400

    Adding Customer Microservice

---
+++ Customer/src/test/java/com/jmendoza/swa/hexagonal/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmendoza.swa.hexagonal.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
