9f7f0b2d19f1e5fe1cb32464d5b9c5ce9639e2c8
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-24 14:48:02 -0400

    Adding Customer Microservice

--- Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/configuration/BeanConfiguration.java
+++ Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/configuration/BeanConfiguration.java
@@ -3,7 +3,6 @@ package com.jmendoza.swa.hexagonal.customer.configuration;
 import com.jmendoza.swa.hexagonal.customer.domain.services.CreateCustomerService;
 import com.jmendoza.swa.hexagonal.customer.infrastracture.adapters.databases.mongo.CustomerRepositoryAdapter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
