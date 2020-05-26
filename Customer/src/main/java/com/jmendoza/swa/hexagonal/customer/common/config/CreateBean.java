3178b323fd05e79775d1b4947bf5cfbb06a9a888
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-26 19:49:56 -0400

    Customer Microservice: Update CustomerLoginUseCase

--- Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/config/CreateBean.java
+++ Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/config/CreateBean.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelMapperConfig {
+public class CreateBean {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
