1c2c0e805537e380e9dbc6ed683c506f2d74efc8
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-26 00:30:04 -0400

    Customer Microservice: Update CustomerLoginUseCase

--- Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/constants/CustomerConstanst.java
+++ Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/constants/CustomerConstanst.java
@@ -2,8 +2,8 @@ package com.jmendoza.swa.hexagonal.customer.common.constants;
 
 public class CustomerConstanst {
     public static final String CUSTOMER_NOT_FOUND = "Customer not found :: ";
-    public static final String THIS_EMAIL_IS_ALREADY_REGISTERED = "This email is already registered.";
-    public static final String THE_PASSWORD_IS_INCORRECT = "The password is incorrect.";
+    public static final String THIS_EMAIL_IS_ALREADY_REGISTERED = "This email is already registered ";
+    public static final String THE_PASSWORD_IS_INCORRECT = "The password is incorrect ";
 
     private CustomerConstanst() {
     }
