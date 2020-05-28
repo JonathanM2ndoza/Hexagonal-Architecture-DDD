886285aa957fcba7244d736b34c155f2479e89bf
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-28 18:09:20 -0400

    Customer Microservice: Add logging log4j2

--- Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/exception/CustomExceptionHandler.java
+++ Customer/src/main/java/com/jmendoza/swa/hexagonal/customer/common/exception/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.jmendoza.swa.hexagonal.customer.common.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,26 +13,26 @@ import java.util.Date;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-    //private static final Logger loggerExceptionHandler = LogManager.getLogger(CustomExceptionHandler.class);
+    private static final Logger loggerException = LogManager.getLogger(CustomExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        //loggerExceptionHandler.error(ex);
+        loggerException.error(ex);
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({GlobalException.class})
     public ResponseEntity globalExceptionHandler(GlobalException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        //loggerExceptionHandler.error(ex);
+        loggerException.error(ex);
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({ParameterNotFoundException.class})
     public ResponseEntity parameterNotFoundExceptionHandler(ParameterNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        //loggerExceptionHandler.error(ex);
+        loggerException.error(ex);
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
