8ee88063a42ad914bafe3b7dfe556914db904461
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-05-31 17:28:11 -0400

    Product Microservice: Update pom.xml

--- Product/src/main/java/com/jmendoza/swa/hexagonal/product/ProductApplication.java
+++ Product/src/main/java/com/jmendoza/swa/hexagonal/product/ProductApplication.java
@@ -3,10 +3,7 @@ package com.jmendoza.swa.hexagonal.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-)
+@SpringBootApplication
 public class ProductApplication {
 
     public static void main(String[] args) {
