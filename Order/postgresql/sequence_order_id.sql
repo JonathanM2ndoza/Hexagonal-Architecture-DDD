41e2c6f5c658d12eae7efbf186b3a1e653904bf0
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-06-02 00:36:05 -0400

    Order Microservice: Update CreateOrderUseCase

---
+++ Order/postgresql/sequence_order_id.sql
@@ -0,0 +1,15 @@
+-- SEQUENCE: public.order_id_seq
+
+-- DROP SEQUENCE public.order_id_seq;
+
+CREATE SEQUENCE public.order_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.order_id_seq
+    OWNER TO postgres;
+
+GRANT ALL ON SEQUENCE public.order_id_seq TO postgres;
