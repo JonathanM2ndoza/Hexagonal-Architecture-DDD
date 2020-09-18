b9581e2080e43434580f1f880efdf7ea0b2472be
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-09-18 18:58:37 -0400

    Order Microservice: Update Infrastructure layer

--- Order/infrastructure/src/main/java/com/jmendoza/swa/hexagonal/infrastracture/databases/postgresql/GetOrderAdapter.java
+++ Order/infrastructure/src/main/java/com/jmendoza/swa/hexagonal/infrastracture/databases/postgresql/GetOrderAdapter.java
@@ -3,47 +3,21 @@ package com.jmendoza.swa.hexagonal.infrastracture.databases.postgresql;
 import com.google.gson.Gson;
 import com.jmendoza.swa.hexagonal.common.exception.GlobalException;
 import com.jmendoza.swa.hexagonal.domain.model.Order;
-import com.jmendoza.swa.hexagonal.domain.ports.outbound.CreateOrderPort;
 import com.jmendoza.swa.hexagonal.domain.ports.outbound.GetOrderPort;
 import org.postgresql.util.PGobject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.Types;
 
-@Repository
-public class OrderRepositoryAdapter implements CreateOrderPort, GetOrderPort {
+@Component
+public class GetOrderAdapter implements GetOrderPort {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
-    @Override
-    public void createOrder(Order order) throws GlobalException {
-
-        final String procedureCall = "{ ? = call create_order(?, ?, ?, ?)}";
-
-        try (Connection connection = jdbcTemplate.getDataSource().getConnection();
-             CallableStatement callableStatement = connection.prepareCall(procedureCall)
-        ) {
-            Gson gson = new Gson();
-            String json = gson.toJson(order.getOrderProductList());
-
-            callableStatement.registerOutParameter(1, Types.BIGINT);
-            callableStatement.setString(2, order.getCustomerId());
-            callableStatement.setTimestamp(3, new java.sql.Timestamp(order.getCreatedAt().getTime()));
-            callableStatement.setDouble(4, order.getAmountOrder());
-            callableStatement.setString(5, json);
-
-            callableStatement.execute();
-
-            order.setOrderId(Long.toString(callableStatement.getLong(1)));
-        } catch (Exception e) {
-            throw new GlobalException("Exception createOrder: " + e.getMessage());
-        }
-    }
 
     @Override
     public Order getOrder(String orderId) throws GlobalException {
